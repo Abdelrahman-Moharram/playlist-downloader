bc6ca4a28722b605bdbc66e47aae174572253407
Abdelrahman-Moharram <abd0m07arram@yahoo.com>
2022-06-04 21:09:48 +0200

    fix issues and add initial work to add thumbainls for original video

--- home/views.py
+++ home/views.py
@@ -5,14 +5,20 @@ from pytube import YouTube
 from pytube.cli import on_progress
 import datetime
 import os
-from .models import Video, save_video
+from .models import Video, save_video, thumbnail_upload
+import pathlib,urllib.request,uuid
+
+
+
+
+
 
 def Download_Playlist(url, v_quality, vs_option):
     playlist = Playlist(url)
     # print(playlist)
     print("Total Videos: ",len(playlist.video_urls))
     folder = str(datetime.datetime.timestamp(datetime.datetime.now()))
-    os.makedirs("media\\files\\"+folder)
+    os.makedirs("media/files/"+folder)
     for video_url in playlist.video_urls:
         yt=YouTube(video_url,on_progress_callback=on_progress)
         if vs_option != "video":
@@ -25,12 +31,12 @@ def Download_Playlist(url, v_quality, vs_option):
             else:
                 stream = yt.streams.filter(resolution=v_quality, res=v_quality).first()
         print(yt.title)
-        stream.download(filename="media\\files\\"+folder+"\\"+yt.title+".mp4")
-    return yt.title, folder, None, "mp4"
+        stream.download(filename="media/files/"+folder+"/"+yt.title+".mp4")
+    return yt.title, folder, None, "mp4", yt.thumbnail_url
 
 def Download_Video(url, v_quality, vs_option):
     folder = str(datetime.datetime.timestamp(datetime.datetime.now()))
-    os.makedirs("media\\files\\"+folder)
+    os.makedirs("media/files/"+folder)
     yt=YouTube(url,on_progress_callback=on_progress)
     if vs_option != "video":
         stream = yt.streams.get_audio_only()
@@ -42,7 +48,7 @@ def Download_Video(url, v_quality, vs_option):
         else:
             stream = yt.streams.filter(resolution=v_quality, res=v_quality).first()
     print(yt.title)
-    return yt.title, folder,  stream.download(filename="media\\files\\"+folder+"\\"+yt.title+".mp4"), "mp4"
+    return yt.title, folder,  stream.download(filename="media/files/"+folder+"/"+yt.title+".mp4"), "mp4", yt.thumbnail_url
 
 
 
@@ -51,36 +57,43 @@ def index(request):
     if request.method == "POST":
         url = request.POST['url']
         if "list" in url :
-            if "index" in url:
-                downloaded = Download_Video(url, request.POST['v-quality'], request.POST['vs-option'])
-                new_video = Video.objects.create(url=request.POST['url'], title=downloaded[0], d_datetime=downloaded[1])
-                new_video.local_src =  save_video(new_video, downloaded[0], ext=downloaded[3])
-                if request.user.username:
-                    new_video.user = request.user
-                
-                new_video.save()
-                print("\n\n\n\n\n",new_video.local_src,"\n\n\n\n\n")
-                return render(request, "home/index.html",{"video":new_video})
-            else:
+            if "index" not in url:
+        
+                # download playlist
+
                 downloaded = Download_Playlist(url, request.POST['v-quality'], request.POST['vs-option'])
                 new_video = Video.objects.create(url=request.POST['url'], title=downloaded[0], d_datetime=downloaded[1])
+                new_video.thumbnail = thumbnail_upload(downloaded[1], filename=downloaded[4].split("/")[-1])
                 new_video.local_src =  save_video(new_video, downloaded[0], ext=downloaded[3])
                 if request.user.username:
                     new_video.user = request.user
                 new_video.save()
                 return render(request, "home/index.html",{"video":new_video})
-        else :
-            downloaded = Download_Video(url, request.POST['v-quality'], request.POST['vs-option'])
-            new_video = Video.objects.create(url=request.POST['url'], title=downloaded[0], d_datetime=downloaded[1])
-            new_video.local_src =  save_video(new_video, downloaded[0], ext=downloaded[3])
-            if request.user.username:
-                new_video.user = request.user
-            new_video.save()
-            print("\n\n\n\n\n",new_video.local_src,"\n\n\n\n\n")
-            return render(request, "home/index.html",{"video":new_video})
         
+
+        # download videos
         
-        
-        print(downloaded)            
-        
+
+
+        downloaded = Download_Video(url, request.POST['v-quality'], request.POST['vs-option'])
+
+        # filename = str(uuid.uuid4())
+        # file_ext = pathlib.Path(downloaded[4]).suffix
+        # picture_filename = filename + file_ext
+        # downloads_path = "media/thumbnails/%s/"%(downloaded[1])
+        # picture_path  = os.path.join(downloads_path, picture_filename)
+        thumbnail,extention = downloaded[4].split("/")[-1].split(".")
+        print("thumbnail,extention = ", thumbnail,extention)
+        urllib.request.urlretrieve(downloaded[4], "media/thumbnails/%s/%s.%s"%(downloaded[1],thumbnail,extention))
+
+
+        new_video = Video.objects.create(url=request.POST['url'], title=downloaded[0], d_datetime=downloaded[1], thumbnail=downloaded[4])
+        new_video.local_src =  save_video(new_video, downloaded[0], ext=downloaded[3])
+        new_video.thumbnail = thumbnail_upload(downloaded[1], filename=downloaded[4].split("/")[-1])
+
+        if request.user.username:
+            new_video.user = request.user
+        new_video.save()
+        return render(request, "home/index.html",{"video":new_video})
+                
     return render(request, "home/index.html",{})
