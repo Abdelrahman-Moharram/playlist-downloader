1bf27f1709dba77258e826d407cd1888e80f5cc3
Abdelrahman Moharram <41553398+Abdelrahman-Moharram@users.noreply.github.com>
2022-06-13 08:10:20 +0200

    fix django heroku lib

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ from decouple import config
 from pathlib import Path
 import os
 from dj_database_url import parse as dburl
-import django_on_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,4 +151,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
