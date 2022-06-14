fe1f7199ba55fe23c51d755ccf866551d266b6f3
Abdelrahman-Moharram <abd0m07arram@yahoo.com>
2022-06-14 03:50:24 +0200

    old commit

--- project/settings.py
+++ project/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-1t5vec@zuh-2s@4&_cd)n13r8l=+!d68)csdxhbp=uzt8*c2o0'
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  True
+DEBUG =  config('DEBUG')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'playlist-video-download.herokuapp.com']
 
@@ -144,8 +143,7 @@ if DEBUG:
             os.path.join(BASE_DIR, 'static')
        ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/' 
 
 
